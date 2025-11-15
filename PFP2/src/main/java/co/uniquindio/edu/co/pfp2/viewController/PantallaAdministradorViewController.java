74669620a0b7e051d5abd5f2ea409301e2ccd15c
JuanSZC <juans.zambranoc@uqvirtual.edu.co>
2025-11-15 15:14:23 -0500

    Update

--- PFP2/src/main/java/co/uniquindio/edu/co/pfp2/viewController/PantallaAdministradorViewController.java
+++ PFP2/src/main/java/co/uniquindio/edu/co/pfp2/viewController/PantallaAdministradorViewController.java
@@ -1,4 +1,21 @@
 package co.uniquindio.edu.co.pfp2.viewController;
 
+import co.uniquindio.edu.co.pfp2.App;
+
 public class PantallaAdministradorViewController {
+
+    App app;
+
+
+
+
+
+
+    public App getApp() {
+        return app;
+    }
+
+    public void setApp(App app) {
+        this.app = app;
+    }
 }
